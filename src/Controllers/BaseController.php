9b59a134712e9b262112df5bb99229a20516d1dc
LuigiTaka <luigimendeszanchett@gmail.com>
2021-11-28 11:36:03 -0300

    feat: Headers e Response Adiciona método para retornar uma resposta com o json no body Método para enviar os cabeçalhos.

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -1,19 +1,25 @@
 <?php
 namespace ProjectReuse\Controllers;
 
+use ProjectReuse\{ArrayUtils,HTTPUtils};
 
 abstract class BaseController
 {
 
-	protected string $requestMethod;
-	protected array $headers = [ ];
+	protected $requestMethod = '';
+	protected $headers = [ ];
 
 
 	function __construct(string $requestMethod)
 	{ 
 		$this->requestMethod = $requestMethod;
 
-		$this->headers =  [ 
+		$this->headers = $this->getDefaultHeaders() ;
+	}
+
+	protected function getDefaultHeaders()
+	{ 
+		return [ 
 			("Access-Control-Allow-Origin: *"),
 	        ("Content-Type: application/json; charset=UTF-8"),
 			("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE"),
@@ -21,18 +27,31 @@ abstract class BaseController
 			("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"),
 		];
 	}
+	
+	protected function flushHeaders()
+	{ 
+		foreach($this->headers as $index => $header)	{ 
+			header($header);
+		}
+	}
 
 	protected function setHeader(array $headers)
 	{ 
 			$this->headers = $headers;
 	}
 
-	protected notFoundResponse() : Response
+	protected function notFoundResponse() : Response
 	{ 
 		return new Response(null,404);
 	}
 
-	abstract processRequest();
+	protected function jsonResponse(array $data,?int $code =  null)
+	{ 
+		$json = ArrayUtils::toJson($data);
+		return new Response($json,$code);
+	}
+
+	abstract function processRequest();
 
 
 	
